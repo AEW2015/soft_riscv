5277eee11490d8abd0c39511587a7a2b81c9d5e7
AEW2015 <lavatech.co@gmail.com>
2018-10-17 17:12:56 -0600

    single

--- bench/simriscv.cpp
+++ bench/simriscv.cpp
@@ -1,4 +1,6 @@
+#include <iostream>
 #include "simriscv.h"
+
 
 SimRISCV::SimRISCV(){
 	pc = 0;
@@ -12,4 +14,28 @@ SimRISCV * SimRISCV::get_sim(){
 	static SimRISCV instance;
 
 	return &instance;
+}
+
+void SimRISCV::reset(){
+	SimRISCV* tmp = get_sim();
+	tmp->pc = 0;
+	std::cout << tmp->pc<<std::endl;
+	for(uint8_t i = 0;i<32;i++){
+		tmp->gpregs[i]=0;
+	}
+}
+void SimRISCV::ori(int rd,int rs1,int imm){
+	SimRISCV* tmp = get_sim();
+	tmp->pc += 4;
+
+	std::cout << tmp->pc<<std::endl;
+	tmp->gpregs[rd] = imm | tmp->gpregs[rs1];
+	std::cout << tmp->gpregs[1]<<std::endl;
+
+}
+int main () {
+	SimRISCV::reset();
+	SimRISCV::ori(1,1,1);
+	SimRISCV::ori(1,1,2);
+	return 0;
 }
--- bench/simriscv.h
+++ bench/simriscv.h
@@ -6,14 +6,14 @@
 class SimRISCV
 {
 private:
-	static SimRISCV *s_instance;
 	SimRISCV();
+
 	static SimRISCV *get_sim();
 	uint32_t pc;
 	uint32_t gpregs[32];	
 public:
-//	static void reset();
-//	static void ori(int rd,int rs1,int imm);
+	static void reset();
+	static void ori(int rd,int rs1,int imm);
 };
 
 
