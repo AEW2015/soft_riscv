b2ca7a3872f2b0ce09d2ca6e4b9cbda15ac82dfc
AEW2015 <lavatech.co@gmail.com>
2018-10-17 16:04:18 -0600

    rtl

---
+++ rtl/cpu_top.v
@@ -0,0 +1,32 @@
+module cpu_top (input logic CLOCK,
+			input typePack::instruction_t INST,
+			output reg[31:0] PC);
+
+
+	reg[31:0] GPREGS[31:0];
+	logic [6:0] test;
+	assign test = INST.itype.opcode;
+	always_ff @(posedge CLOCK) begin
+		PC <= PC +1;
+	end
+
+	always_ff @(posedge CLOCK) begin
+		unique case(INST.itype.opcode)
+			typePack::IMM:
+				unique case(INST.itype.funct3)
+					typePack::ORI:
+						GPREGS[INST.itype.rd] = INST.itype.imm | GPREGS[INST.itype.rs1];
+						//do nothing
+				endcase
+			typePack::OMM:
+				GPREGS[1] <= 0;
+			default:
+				GPREGS[1] <= 0;
+		endcase
+
+	end
+
+
+
+endmodule
+
---
+++ rtl/typePack.v
@@ -0,0 +1,39 @@
+package typePack;
+
+typedef enum logic[6:0]{
+	IMM = 7'b0010011,
+	OMM = 7'b0000011
+}
+opcodes /* verilator public */;
+
+typedef enum logic[2:0]{
+	ADDI = 3'b000,
+	SLTI = 3'b010,
+	SLTIU = 3'b011,
+	XORI = 3'b100,
+	ORI = 3'b110,
+	ANDI = 3'b111
+} FIMM  /* verilator public */;
+
+localparam ASUB = 7'b0100000;
+
+
+
+typedef struct packed
+{
+	logic[11:0] imm;
+	logic [4:0] rs1;
+	logic [2:0] funct3;
+	logic [4:0] rd;
+	opcodes opcode;
+
+
+} ITYPE_T  /* verilator public */;
+
+typedef union packed
+{
+  ITYPE_T itype;
+} instruction_t /* verilator public */;
+
+
+endpackage : typePack
